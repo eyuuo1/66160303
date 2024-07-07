4eb5877d74c5607ed12db40fcbd91bc7785b80f6
eyuuo1 <66160303@go.buu.ac.th>
2024-07-07 23:51:32 +0700

    oxgame.java

---
+++ oxgame.java
@@ -0,0 +1,16 @@
+# 66160303
+  import java.util.Scanner;
+public class XO {
+    public static void main(String[] args) {
+        System.out.println("Welcome to XO");
+        for(int i = 0; i<3 ; i++){
+            for(int j =0;j<3;j++){
+                System.out.print("_ ");
+            }
+            System.out.println("");
+        }
+        System.out.println("Turn X input (row,col)");
+        
+    }
+            
+}
